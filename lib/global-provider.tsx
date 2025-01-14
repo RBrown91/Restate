8d71b545772e7751a81882f1ba0f39d80881d827
Lunchbox91 <ryanallenbrown@gmail.com>
2025-01-14 18:46:47 +0000

    Home page layout, styling and filter logic.

--- lib/global-provider.tsx
+++ lib/global-provider.tsx
@@ -40,7 +40,7 @@ export const useGlobalContext = (): GlobalContextType => {
   const context = useContext(GlobalContext);
 
   if (!context) {
-    throw new Error("useGlobalContext must be used within a GlobalProdiver");
+    throw new Error("useGlobalContext must be used within a GlobalProvider");
   }
 
   return context;
